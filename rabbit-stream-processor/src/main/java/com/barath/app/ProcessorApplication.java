d7dfefa4ff7ca1d212064a588de9f5a006a1ca20
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-08-30 21:49:00 +0530

    adding main classes

--- rabbit-stream-processor/src/main/java/com/barath/app/ProcessorApplication.java
+++ rabbit-stream-processor/src/main/java/com/barath/app/ProcessorApplication.java
@@ -3,14 +3,14 @@ package com.barath.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
-@EnableBinding(Sink.class)
-public class SinkApplication {
+@EnableBinding(Processor.class)
+public class ProcessorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SinkApplication.class, args);
+		SpringApplication.run(ProcessorApplication.class, args);
 	}
 }
