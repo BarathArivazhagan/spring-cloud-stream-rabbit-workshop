aaeb9edd398e26102b0079944329c2c551e15c28
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-08-30 21:40:06 +0530

    adding source,sink,processor modules

---
+++ rabbit-stream-source/src/main/java/com/barath/cloud/stream/samples/rabbitstreamsource/RabbitStreamSourceApplication.java
@@ -0,0 +1,12 @@
+package com.barath.cloud.stream.samples.rabbitstreamsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitStreamSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitStreamSourceApplication.class, args);
+	}
+}
---
+++ rabbit-stream-source/src/test/java/com/barath/cloud/stream/samples/rabbitstreamsource/RabbitStreamSourceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.barath.cloud.stream.samples.rabbitstreamsource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitStreamSourceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
